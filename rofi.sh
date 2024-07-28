02d47cca8e1b3f74fe0dc56f9202e567bc4e3e9d
wancat <wancat@wancat.cc>
2024-07-28 13:41:26 +0800

    remove full-text search, simplify

--- rofi.sh
+++ rofi.sh
@@ -4,22 +4,10 @@
 CHEATSHEET_DIR="$HOME/cheatsheets"
 
 # Use rofi to get the search query from the user
-SEARCH_QUERY=$(rofi -dmenu -i -p "Search Cheatsheets")
+SELECTED_FILE=$(find $CHEATSHEET_DIR -name "*.md" | rofi -dmenu -i -p "Search Cheatsheets")
 
-# If the user entered a search query, perform the search
-if [ -n "$SEARCH_QUERY" ]; then
-    # Perform the search and store results
-    SEARCH_RESULTS=$(grep -rni "$SEARCH_QUERY" "$CHEATSHEET_DIR" --include "*.md")
+echo $SELECTED_FILE
 
-    # Display the search results in rofi and get the selected line
-    SELECTED_RESULT=$(echo "$SEARCH_RESULTS" | rofi -dmenu -i -p "Search Results")
-
-    # Extract the file path from the selected result
-    SELECTED_FILE=$(echo "$SELECTED_RESULT" | awk -F: '{print $1}')
-
-    # Open the selected file in the text editor if a file was selected
-    if [ -n "$SELECTED_FILE" ]; then
-        xed "$SELECTED_FILE"  # Replace 'gedit' with your preferred text editor
-    fi
+if [ -n "$SELECTED_FILE" ]; then
+    xed "$SELECTED_FILE"  # Replace 'gedit' with your preferred text editor
 fi
-
